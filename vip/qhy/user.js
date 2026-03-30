75fc988c6caa66510cb764c551af7f33b42db2ee
ElvisJENSEN <103517327+ElvisJENSEN@users.noreply.github.com>
2026-03-30 03:41:20 +0000

    change qhy

--- vip/qhy/user.js
+++ vip/qhy/user.js
@@ -3,6 +3,8 @@ var obj = JSON.parse(body);
 
 obj['data']['vipExpireTime'] = "2036-05-05 19:01:33";
 obj['data']['expire'] = "2036年5月5日";
+obj['data']['levelCn'] = "会员用户";
+obj['data']['level'] = "3";
 body = JSON.stringify(obj);
 
 $done(body);
